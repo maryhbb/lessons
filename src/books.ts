433def554c66def4514d8c28baf1c6fef6f389f4
Mary <mary_hbb@yahoo.com>
2023-02-12 22:06:58 +0100

    Done countBooks function

--- src/books.ts
+++ src/books.ts
@@ -1,9 +1,15 @@
 import { Book } from "../types";
 
 export function countBooks(books: Book[]): number {
-    return 0; // to be implemented
+    return books.length;
 }
 
 export function findUniqueCountries(books: Book[]): string[] {
+
+    for (const book of books) {
+        console.log(book.country)
+
+    }
     return []; // to be implemented
+
 }
