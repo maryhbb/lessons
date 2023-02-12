cf3bd01138b1ca06b4dfac0d8446c4903d55da78
Mary <mary_hbb@yahoo.com>
2023-02-12 22:56:44 +0100

    Done findUniqueCountries funtion

--- src/books.ts
+++ src/books.ts
@@ -1,15 +1,18 @@
-import { Book } from "../types";
+import { Book } from '../types';
 
 export function countBooks(books: Book[]): number {
     return books.length;
 }
 
 export function findUniqueCountries(books: Book[]): string[] {
+    // for (const book of books) {
+    //     console.log(book.country)
+    // }
 
-    for (const book of books) {
-        console.log(book.country)
-
-    }
-    return []; // to be implemented
-
+    let countries = books.map(book => book.country);
+    console.log(countries);
+    let uniqueCountries = countries.filter(
+        (value, index, arr) => arr.indexOf(value) === index,
+    );
+    return uniqueCountries;
 }
